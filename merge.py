0699942fe0d7aa5ae083c5481821a08c11a5f1b8
Xennis <code@xennis.org>
2024-06-06 18:48:39 +0200

    Rework merging

--- merge.py
+++ merge.py
@@ -10,16 +10,16 @@ csv_fieldnames = [
     "commentary",
     "mainLang",
     "foreignLang",
-    "origin_dates_when",
-    "origin_dates_notbefore",
-    "origin_dates_notafter",
-    "origin_places",
-    "provenances_located",
-    "provenances_found",
+    "originDatesWhen",
+    "originDatesNotbefore",
+    "originDatesNotafter",
+    "originPlaces",
+    "provenancesLocated",
+    "provenancesFound",
     # Information about the source of this entry
-    "source_files",
-    "source_authority",
-    "source_availability",
+    "sourceFiles",
+    "sourceAuthority",
+    "sourceAvailability",
 ]
 
 
@@ -39,23 +39,52 @@ def create_list(elements, field: str) -> Optional[list[Any]]:
     return result
 
 
-def merge(result: dict[str, Any], field: str, value: Any) -> None:
+def merge_single(result: dict[str, Any], field: str, value: Optional[Any]) -> None:
+    if value is None:
+        return
+
     current = result.get(field)
     if current is None:
         result[field] = value
         return
 
-    if current == value or value is None:
+    merged_before = isinstance(current, list)
+    if not merged_before:
+        if current == value:
+            return  # Same value
+
+        result[field] = [current, value]
+    else:
+        if any(value == c for c in current):
+            return  # Same value
+
+        result[field] = current.append(value)
+
+
+def merge_list(result: dict[str, Any], field: str, value: Optional[list[Any]]) -> None:
+    if value is None:
         return
 
-    result[field] = [current, value]
+    current = result.get(field)
+    if current is None:
+        result[field] = value
+        return
+
+    merged_before = len(current) > 0 and isinstance(current[0], list)
+    if not merged_before:
+        if current == value:
+            return
+
+        result[field] = [current, value]
+    else:
+        # FIXME: Here is no check if the list already exists
+        result[field] = current.extend(value)
 
 
 def convert(tm: str, files: list[str], idp_data_repo: str) -> dict[str, Any]:
     result = {
         "tm": tm,
-        # "trismegistos_url": f"https://www.trismegistos.org/text/{tm}",
-        "source_files": files,
+        "sourceFiles": files,
     }
     for file in files:
 
@@ -74,17 +103,17 @@ def convert(tm: str, files: list[str], idp_data_repo: str) -> dict[str, Any]:
             pass
         elif file.startswith("DCLP"):
             # url: f"http://papyri.info/dclp/{tm}"
-            merge(result, "terms", [term.get("text") for term in doc.terms])
-            merge(result, "title", doc.title)
-            merge(result, "material", doc.material)
-            merge(result, "origin_dates_when", create_list(doc.origin_dates, "when"))
-            merge(result, "origin_dates_notbefore", create_list(doc.origin_dates, "notbefore"))
-            merge(result, "origin_dates_notafter", create_list(doc.origin_dates, "notafter"))
-            merge(result, "origin_places", doc.origin_place.get("text"))
-            merge(result, "provenances_located", create_list(doc.provenances.get("located"), "text"))
-            merge(result, "provenances_found", create_list(doc.provenances.get("found"), "text"))
-            merge(result, "source_authority", doc.authority)
-            merge(result, "source_availability", doc.availability)
+            merge_list(result, "terms", [term.get("text") for term in doc.terms])
+            merge_single(result, "title", doc.title)
+            merge_single(result, "material", doc.material)
+            merge_list(result, "originDatesWhen", create_list(doc.origin_dates, "when"))
+            merge_list(result, "originDatesNotbefore", create_list(doc.origin_dates, "notbefore"))
+            merge_list(result, "originDatesNotafter", create_list(doc.origin_dates, "notafter"))
+            merge_single(result, "originPlaces", doc.origin_place.get("text"))
+            merge_list(result, "provenancesLocated", create_list(doc.provenances.get("located"), "text"))
+            merge_list(result, "provenancesFound", create_list(doc.provenances.get("found"), "text"))
+            merge_single(result, "sourceAuthority", doc.authority)
+            merge_single(result, "sourceAvailability", doc.availability)
         elif file.startswith("DDB_EpiDoc_XML"):
             # url: f"http://papyri.info/ddbdp/{ddb_hybrid}" with `ddb_hybrid = doc.idno.get("ddb-hybrid")`
 
@@ -92,26 +121,26 @@ def convert(tm: str, files: list[str], idp_data_repo: str) -> dict[str, Any]:
             # if 'en' in lang_usage:
             #     lang_usage.remove('en')
             # result['langUsage'] = lang_usage
-            merge(result, "mainLang", doc.edition_language)
-            merge(result, "foreignLang", doc.edition_foreign_languages if doc.edition_foreign_languages else None)
+            merge_single(result, "mainLang", doc.edition_language)
+            merge_single(result, "foreignLang", doc.edition_foreign_languages if doc.edition_foreign_languages else None)
             # 'sources' field used instead of:
             # merge(result, "ddb_url", f"http://papyri.info/ddbdp/{ddb_hybrid}")
-            merge(result, "source_authority", doc.authority)
-            merge(result, "source_availability", doc.availability)
+            merge_list(result, "sourceAuthority", doc.authority)
+            merge_list(result, "sourceAvailability", doc.availability)
         elif file.startswith("HGV_meta_EpiDoc"):
             # url: f"http://papyri.info/hgv/{tm}"
-            merge(result, "terms", [term.get("text") for term in doc.terms])
-            merge(result, "title", doc.title)
-            merge(result, "commentary", doc.commentary)
-            merge(result, "material", doc.material)
-            merge(result, "origin_dates_when", create_list(doc.origin_dates, "when"))
-            merge(result, "origin_dates_notbefore", create_list(doc.origin_dates, "notbefore"))
-            merge(result, "origin_dates_notafter", create_list(doc.origin_dates, "notafter"))
-            merge(result, "origin_places", doc.origin_place.get("text"))
-            merge(result, "provenances_located", create_list(doc.provenances.get("located"), "text"))
-            merge(result, "provenances_found", create_list(doc.provenances.get("found"), "text"))
-            merge(result, "source_authority", doc.authority)
-            merge(result, "source_availability", doc.availability)
+            merge_list(result, "terms", [term.get("text") for term in doc.terms])
+            merge_single(result, "title", doc.title)
+            merge_single(result, "commentary", doc.commentary)
+            merge_single(result, "material", doc.material)
+            merge_list(result, "originDatesWhen", create_list(doc.origin_dates, "when"))
+            merge_list(result, "originDatesNotbefore", create_list(doc.origin_dates, "notbefore"))
+            merge_list(result, "originDatesNotafter", create_list(doc.origin_dates, "notafter"))
+            merge_single(result, "originPlaces", doc.origin_place.get("text"))
+            merge_list(result, "provenancesLocated", create_list(doc.provenances.get("located"), "text"))
+            merge_list(result, "provenancesFound", create_list(doc.provenances.get("found"), "text"))
+            merge_single(result, "sourceAuthority", doc.authority)
+            merge_single(result, "sourceAvailability", doc.availability)
         elif file.startswith("HGV_trans_EpiDoc"):
             pass
 
