b5fb071379004028a215ccdc6f0a723b337cc633
Fabian Rosenthal <code@xennis.org>
2026-08-04 10:27:58 +0200

    fix(cd): Use TS CLI due to TS v7 update (#82)

--- web/next.config.ts
+++ web/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   basePath: "/idp.data-search",
   output: "export",
+  experimental: {
+    useTypeScriptCli: true,
+  },
 }
 
 export default nextConfig
